095601cf7988929dd9b974697e5bdd469dfe446e
unknown <b3kurger@vasvari.org>
2025-12-09 17:36:23 +0100

    Bekérés lekezelése

---
+++ robotporszivo_ikt_kurger/robotporszivo_ikt_kurger/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace robotporszivo_ikt_kurger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0, m = 0; // N sor, M oszlop
+            do
+            {
+                Console.Write("Adja meg hány sorból álljon a tömb (20-30, sorok és oszlopok NEM = ):");
+                n = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Adja meg hány álljon a tömb (20-30, sorok és oszlopok NEM = ):");
+                m = Convert.ToInt32(Console.ReadLine());
+                if (n < 20 || n > 30 || m < 20 || m > 30 || n == m)
+                {
+                    Console.WriteLine("Nem megfelelő értéktartományban adott összeget!");
+                }
+
+            } while (n < 20 || n > 30 || m < 20 || m > 30 || n == m);
+            int[,] lakas = new int[n, m];
+        }
+    }
+}
