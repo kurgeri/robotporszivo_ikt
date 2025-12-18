191c5198336d3831531547a3a4478c3294065f9f
unknown <b3kurger@vasvari.org>
2025-12-18 19:42:12 +0100

    Egy szabad és egy koszos terület biztosítása

--- robotporszivo_ikt_kurger/robotporszivo_ikt_kurger/Program.cs
+++ robotporszivo_ikt_kurger/robotporszivo_ikt_kurger/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,23 +26,25 @@ namespace robotporszivo_ikt_kurger
             } while (n < 20 || n > 30 || m < 20 || m > 30 || n == m);
             char[,] lakas = new char[n, m];
             Feltolt(lakas);
-           
+            Clanker(lakas);
+
         }
 
         static void Feltolt(char[,] t)
         {
-
+            bool koszos = false, szabad = false;
             Random rnd = new Random();
             for (int i = 0; i < t.GetLength(0); i++)
-        {
-           for (int j = 0 ; j < t.GetLength(1); j++)
-           {
-                   
+            {
+                for (int j = 0; j < t.GetLength(1); j++)
+                {
+
                     int szam = rnd.Next(1, 11);
-                   
+
                     if (szam <= 5) // szabad
                     {
                         t[i, j] = '_';
+                        szabad = true;
                     }
                     else if (szam <= 7) // bútor
                     {
@@ -50,14 +53,55 @@ namespace robotporszivo_ikt_kurger
                     else // koszos
                     {
                         t[i, j] = 'k';
+                        koszos = true;
                     }
+                   
 
-                    Console.Write($"{t[i, j]}  ");  
-           }
-           Console.WriteLine();
+
+                }
+               
+
+            }
+
+            if (!koszos || !szabad)
+            {
+                Feltolt(t);
+            }
         }
+
+        static void Clanker(char[,] t)
+        {
         
-        
+            List<int[,]> ureshelyek = new List<int[,]>();
+
+            Random rnd = new Random();
+      
+            for (int i = 0; i < t.GetLength(0); i++)
+            {
+                for (int j = 0; j < t.GetLength(1); j++)
+                {
+
+                 
+                    if (t[i, j] == '_')
+                    {
+                       // robot elhelyezését bekéne fejezni te kaki
+
+
+
+
+
+
+                    }
+                    if (t[i, j] == 'r')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    Console.Write($"{t[i, j]}  ");
+                    Console.ResetColor();   
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
